1f2b5c968ab179bf7fd68fb190c2f9a2c27825e3
HE, Tao <sighingnow@gmail.com>
2018-07-17 15:03:24 +0800

    Fix activation layers in both generator and discriminator in LSGAN.
    Signed-off-by: HE, Tao <sighingnow@gmail.com>

--- lsgan.py
+++ lsgan.py
@@ -34,7 +34,7 @@ class Reshape(nn.Module):
         self.shape = size
     # pylint: disable=arguments-differ
     def forward(self, x):
-        return x.view(-1, *self.shape)
+        return x.view(x.size(0), *self.shape)
 
 class Generator(nn.Module):
     def __init__(self, config):
@@ -44,19 +44,27 @@ class Generator(nn.Module):
             nn.Linear(config.in_channels, 256 * 7 * 7),     # 256 x 7 x 7
             Reshape(256, 7, 7),
             nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.ConvTranspose2d(256, 256, 3, 2, bias=False), # 256 x 15 x 15
             nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.ConvTranspose2d(256, 256, 3, 1, bias=False), # 256 x 17 x 17
             nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.ConvTranspose2d(256, 256, 3, 2, bias=False), # 256 x 35 x 35
             nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.ConvTranspose2d(256, 256, 3, 1, bias=False), # 256 x 37 x 37
             nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.ConvTranspose2d(256, 128, 3, 2, bias=False), # 128 x 75 x 75
             nn.BatchNorm2d(128),
+            nn.ReLU(),
             nn.ConvTranspose2d(128, 64, 3, 2, bias=False),  # 64 x 151 x 151
             nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 1, 3, 1, bias=False)     # 1 x 153 x 153
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 1, 3, 1, bias=False),    # 1 x 153 x 153
+            nn.Tanh(),
         )
 
     def forward(self, *x):
@@ -72,12 +80,15 @@ class Discriminator(nn.Module):
             nn.Conv2d(64, 128,
                       5, 2, 0, bias=False), # 128 x 36 x 36
             nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(128, 256,
                       5, 2, 0, bias=False), # 256 x 16 x 16
             nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(256, 512,
                       5, 2, 0, bias=False), # 512 x 6 x 6
             nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
             Flatten(),                      # (512 x 6 x 6)
             nn.Linear(512 * 6 * 6, 1),      # 1
         )
@@ -134,17 +145,20 @@ class LSGAN(object):
                                      shuffle=True,
                                      num_workers=2)
 
-    def train(self):
+    def train(self, num_epoch=None, start_epoch=1):
+        if num_epoch is None:
+            num_epoch = self.config.num_epoch
+
         # used to inspect the intermedia result of the models.
         fixed_noise = torch.randn(self.config.batch_size, self.config.g.in_channels,
                                   device=self.config.device)
 
-        for epoch in range(1, self.config.num_epoch + 1):
+        for epoch in range(start_epoch, num_epoch + start_epoch):
             for _i, data in enumerate(self.dataset, 0):
                 self.D.zero_grad()
 
                 real_data = data[0].to(self.config.device)
-                batch_size = real_data.size()[0]
+                batch_size = real_data.size(0)
                 noise = torch.randn(batch_size, self.config.g.in_channels,
                                     device=self.config.device)
                 fake_data = self.G(noise)
@@ -173,7 +187,7 @@ class LSGAN(object):
                 self.G.eval()
                 fixed_fake_data = self.G(fixed_noise)
                 self.G.train()
-                vutils.save_image(fixed_fake_data.detach(), 'fake_samples_epoch_%03d.png' % epoch, normalize=True)
+                vutils.save_image(fixed_fake_data.detach(), 'lsgan_fake_samples_epoch_%03d.png' % epoch, normalize=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cuda', action='store_true')
@@ -182,8 +196,9 @@ parser.add_argument('--verbose', action='store_true')
 if __name__ == '__main__':
     logger = logging.getLogger('LSGAN')
     logger.setLevel(logging.INFO)
-    logger.handlers = [logging.StreamHandler(sys.stderr)]
-    logger.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(logging.Formatter('%(asctime)s[%(name)s]%(levelname)s - %(message)s', '%m-%d %H:%M:%S'))
+    logger.handlers = [handler]
 
     opt = parser.parse_args()
 
@@ -208,7 +223,7 @@ if __name__ == '__main__':
         },
         'device': device,
         'batch_size': 64,
-        'num_epoch': 5,
+        'num_epoch': 10,
         'print_interval': 5,
         'verbose': opt.verbose,
         'datadir': 'dataset/data/mnist',
